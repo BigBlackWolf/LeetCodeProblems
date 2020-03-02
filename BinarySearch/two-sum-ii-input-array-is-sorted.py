a92e6d5a82c6d96b3a5968680acaf2a3217a4d8f
BigBlackWolf <stepan.rasputny@gmail.com>
2020-03-02 09:57:01 +0200

    Easy (52,7%)

---
+++ BinarySearch/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,30 @@
+"""
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+    Your returned answers (both index1 and index2) are not zero-based.
+    You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+"""
+
+
+class Solution:
+    def twoSum(self, numbers, target: int):
+        start, finish = 0, len(numbers) - 1
+        while start != finish:
+            if numbers[finish] + numbers[start] == target:
+                return [start + 1, finish + 1]
+            elif numbers[finish] > target:
+                finish -= 1
+            elif numbers[finish] + numbers[start] < target:
+                start += 1
+            else:
+                finish -= 1
